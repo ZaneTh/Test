092571768695e7cdc5e895d62eea614462ae7231
ZaneTh <67547370+ZaneTh@users.noreply.github.com>
2021-07-01 14:59:21 +0200

    Add files via upload

---
+++ C++/new.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+//    std::cout <<"Hello, World!";
+//    int firstNumber, secondNumber, sumOfTwoNumbers;
+
+//    cout << "Enter first integers: ";
+//    cin >> firstNumber ;
+//    cout << "Enter second integers: ";
+//    cin >> secondNumber;
+    // sum of two numbers in stored in variable sumOfTwoNumbers
+//    sumOfTwoNumbers = firstNumber + secondNumber;
+
+    // Prints sum
+//    cout << firstNumber << " + " << secondNumber << " = " << sumOfTwoNumbers;
+//    return 0;
+
+//    int divisor, dividend, quotient, remainder;
+
+//    cout << "Enter dividend: ";
+//    cin >> dividend;
+
+//    cout << "Enter divisor: ";
+//    cin >> divisor;
+
+//    quotient = dividend/divisor;
+//    remainder = dividend % divisor;
+
+//    cout << "Quotient = " << quotient << endl;
+//    cout << "Remainder = " << remainder;
+
+//    return 0;
+
+//    cout << "Size of char: " << sizeof(char) << "byte" << endl;
+//    cout << "Size of int: " << sizeof(int) << "byte" << endl;
+//    cout << "Size of float: " << sizeof(float) << "byte" << endl;
+//    cout << "Size of double: " << sizeof(double) << "byte" << endl;
+
+    int a = 5, b = 10, temp;
+
+    cout << "Before swapping." << endl;
+    cout << "a = " << a << ", b = " << b << endl;
+
+    temp = a;
+    a = b;
+    b = temp;
+
+    cout << "\n After swapping." << endl;
+    cout << "a = " << a << ", b = " << b << endl;
+
+    return 0;
+}
+
